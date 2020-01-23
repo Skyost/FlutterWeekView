f91857a38154e1ef78a13bc4f001e51efee0f08f
Skyost <me@skyost.eu>
2020-01-23 22:21:32 +0100

    Now using Dart lint in example too.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart' as URLLauncher;
+import 'package:url_launcher/url_launcher.dart' as launcher;
 import 'package:flutter_week_view/flutter_week_view.dart';
 
 /// First plugin test method.
@@ -42,14 +42,14 @@ class _FlutterWeekViewDemoAppBody extends StatelessWidget {
   Widget build(BuildContext context) {
     String github = 'https://github.com/Skyost/FlutterWeekView';
     return Padding(
-      padding: EdgeInsets.all(30),
+      padding: const EdgeInsets.all(30),
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Padding(
-              padding: EdgeInsets.only(bottom: 40),
+              padding: const EdgeInsets.only(bottom: 40),
               child: Text(
                 'Flutter Week View demo',
                 style: Theme.of(context).textTheme.display1,
@@ -57,20 +57,20 @@ class _FlutterWeekViewDemoAppBody extends StatelessWidget {
               ),
             ),
             RaisedButton(
-              child: Text('Demo day view'),
+              child: const Text('Demo day view'),
               onPressed: () => Navigator.pushNamed(context, '/day-view'),
             ),
             RaisedButton(
-              child: Text('Demo week view'),
+              child: const Text('Demo week view'),
               onPressed: () => Navigator.pushNamed(context, '/week-view'),
             ),
-            Expanded(
+            const Expanded(
               child: SizedBox.expand(),
             ),
             GestureDetector(
               onTap: () async {
-                if (await URLLauncher.canLaunch(github)) {
-                  await URLLauncher.launch(github);
+                if (await launcher.canLaunch(github)) {
+                  await launcher.launch(github);
                 }
               },
               child: Text(
@@ -101,32 +101,32 @@ class _DemoDayView extends StatelessWidget {
         FlutterWeekViewEvent(
           title: 'An event 1',
           description: 'A description 1',
-          start: date.subtract(Duration(hours: 1)),
-          end: date.add(Duration(hours: 18, minutes: 30)),
+          start: date.subtract(const Duration(hours: 1)),
+          end: date.add(const Duration(hours: 18, minutes: 30)),
         ),
         FlutterWeekViewEvent(
           title: 'An event 2',
           description: 'A description 2',
-          start: date.add(Duration(hours: 19)),
-          end: date.add(Duration(hours: 22)),
+          start: date.add(const Duration(hours: 19)),
+          end: date.add(const Duration(hours: 22)),
         ),
         FlutterWeekViewEvent(
           title: 'An event 3',
           description: 'A description 3',
-          start: date.add(Duration(hours: 23, minutes: 30)),
-          end: date.add(Duration(hours: 25, minutes: 30)),
+          start: date.add(const Duration(hours: 23, minutes: 30)),
+          end: date.add(const Duration(hours: 25, minutes: 30)),
         ),
         FlutterWeekViewEvent(
           title: 'An event 4',
           description: 'A description 4',
-          start: date.add(Duration(hours: 20)),
-          end: date.add(Duration(hours: 21)),
+          start: date.add(const Duration(hours: 20)),
+          end: date.add(const Duration(hours: 21)),
         ),
         FlutterWeekViewEvent(
           title: 'An event 5',
           description: 'A description 5',
-          start: date.add(Duration(hours: 20)),
-          end: date.add(Duration(hours: 21)),
+          start: date.add(const Duration(hours: 20)),
+          end: date.add(const Duration(hours: 21)),
         ),
       ],
       currentTimeCircleColor: Colors.pink,
@@ -141,37 +141,37 @@ class _DemoWeekView extends StatelessWidget {
     DateTime now = DateTime.now();
     DateTime date = DateTime(now.year, now.month, now.day);
     return WeekView(
-      dates: [date.subtract(Duration(days: 1)), date, date.add(Duration(days: 1))],
+      dates: [date.subtract(const Duration(days: 1)), date, date.add(const Duration(days: 1))],
       events: [
         FlutterWeekViewEvent(
           title: 'An event 1',
           description: 'A description 1',
-          start: date.subtract(Duration(hours: 1)),
-          end: date.add(Duration(hours: 18, minutes: 30)),
+          start: date.subtract(const Duration(hours: 1)),
+          end: date.add(const Duration(hours: 18, minutes: 30)),
         ),
         FlutterWeekViewEvent(
           title: 'An event 2',
           description: 'A description 2',
-          start: date.add(Duration(hours: 19)),
-          end: date.add(Duration(hours: 22)),
+          start: date.add(const Duration(hours: 19)),
+          end: date.add(const Duration(hours: 22)),
         ),
         FlutterWeekViewEvent(
           title: 'An event 3',
           description: 'A description 3',
-          start: date.add(Duration(hours: 23, minutes: 30)),
-          end: date.add(Duration(hours: 25, minutes: 30)),
+          start: date.add(const Duration(hours: 23, minutes: 30)),
+          end: date.add(const Duration(hours: 25, minutes: 30)),
         ),
         FlutterWeekViewEvent(
           title: 'An event 4',
           description: 'A description 4',
-          start: date.add(Duration(hours: 20)),
-          end: date.add(Duration(hours: 21)),
+          start: date.add(const Duration(hours: 20)),
+          end: date.add(const Duration(hours: 21)),
         ),
         FlutterWeekViewEvent(
           title: 'An event 5',
           description: 'A description 5',
-          start: date.add(Duration(hours: 20)),
-          end: date.add(Duration(hours: 21)),
+          start: date.add(const Duration(hours: 20)),
+          end: date.add(const Duration(hours: 21)),
         ),
       ],
     );
