5bf642cf5c02ab09dd2e5b8d173138b1359ac13b
Skyost <me@skyost.eu>
2020-02-02 13:44:24 +0100

    Fixed a problem with controllers not being exported.

--- lib/flutter_week_view.dart
+++ lib/flutter_week_view.dart
@@ -1,6 +1,7 @@
 /// Flutter Week View, created by Skyost
 /// Github : https://github.com/Skyost/FlutterWeekView
 
+export 'package:flutter_week_view/src/controller.dart';
 export 'package:flutter_week_view/src/day_view.dart';
 export 'package:flutter_week_view/src/event.dart';
 export 'package:flutter_week_view/src/headers.dart';
